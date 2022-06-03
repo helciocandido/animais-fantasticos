6579e6a095b6f1a4afd68a18b6e7fb868d94a765
Helcio Candido <helcio.mcandido@gmail.com>
2022-06-03 13:22:08 -0400

    ultimo save do refazer animação scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -6,6 +6,8 @@ export default class ScrollAnima {
     this.checkDistance = this.checkDistance.bind(this);
   }
 
+  //Pega a distancia de cada item
+  //em relação ao topo do site
   getDistance() {
     this.distance = [...this.sections].map((section) => {
       const offset = section.offsetTop;
@@ -16,6 +18,8 @@ export default class ScrollAnima {
     });
   }
 
+  //Verifica a distancia em cada objeto
+  //em relação ao scroll do site
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
@@ -35,6 +39,7 @@ export default class ScrollAnima {
     return this;
   }
 
+  //Remove o evento de scroll
   stop() {
     window.removeEventListener("scroll", this.checkDistance);
   }
